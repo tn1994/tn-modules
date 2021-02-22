1b4c40bfe74340a9e3a0a2509c7125f1038dd40d
tnosho <takamichi.nosho@hightec.co.jp>
2021-02-23 02:15:11 +0900

    feature: cp file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from python_session.for_file import hoge
 from python_session.for_file import join_filename
+from python_session.for_file import join_and_cp
 
-hoge()
-join_filename()
+# hoge()
+# join_filename()
+join_and_cp()
--- python_session/for_file.py
+++ python_session/for_file.py
@@ -1,10 +1,30 @@
 import glob
+import shutil
+import pathlib
+
 
 def hoge():
     return print('hoge')
+
 
 def join_filename():
     list = glob.glob('**/*', recursive=True)
     for i in list:
         split_list = i.split('/')
         print('_'.join(split_list))
+
+
+def join_and_cp():
+    output_dir_path = 'archive'
+    list = glob.glob('**/*', recursive=True)
+    for i in list:
+        if is_file(i):
+            split_list = i.split('/')
+            rename_filename = '_'.join(split_list)
+            shutil.copy(i, pathlib.Path(output_dir_path, rename_filename))
+
+
+def is_file(path):
+    if isinstance(path, str):
+        path = pathlib.Path(path)
+    return path.suffix
